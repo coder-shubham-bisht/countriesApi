597605ddc8f719e03f3756856d656ccc9aebef16
Shubham Bisht <bistshubham2016@gmail.com>
2024-03-11 13:01:08 +0530

    countryApicomplete

---
+++ script.js
@@ -0,0 +1,59 @@
+const countriesContainer = document.querySelector('.countries-container')
+const filterByRegion = document.querySelector('.filter-by-region')
+const searchInput = document.querySelector('.search-container input')
+const themeChanger = document.querySelector('.theme-changer')
+
+let allCountriesData;
+
+fetch('https://restcountries.com/v3.1/all')
+  .then((res) => res.json())
+  .then((data) => {
+    renderCountries(data)
+    allCountriesData = data
+  })
+
+filterByRegion.addEventListener('change', (e) => {
+//   fetch(`https://restcountries.com/v3.1/region/${filterByRegion.value}`)
+//     .then((res) => res.json())
+//     .then(renderCountries)
+const filteredCountries = allCountriesData.filter((country) => country.region.toLowerCase().includes(filterByRegion.value.toLowerCase()))
+renderCountries(filteredCountries)
+
+})
+
+function renderCountries(data) {
+  countriesContainer.innerHTML = ''
+  if(data.length<1){
+    console.log(data);
+    countriesContainer.innerHTML = `<h2> No Country Found </h2>`
+    return
+  }
+  data.forEach((country) => {
+    const countryCard = document.createElement('a')
+    countryCard.classList.add('country-card')
+    countryCard.href = `/country.html?name=${country.name.common}`
+    countryCard.innerHTML = `
+          <img src="${country.flags.svg}" alt="${country.name.common} flag" />
+          <div class="card-text">
+              <h3 class="card-title">${country.name.common}</h3>
+              <p><b>Population: </b>${country.population.toLocaleString(
+                'en-IN'
+              )}</p>
+              <p><b>Region: </b>${country.region}</p>
+              <p><b>Capital: </b>${country.capital?.[0]}</p>
+          </div>
+  `
+    countriesContainer.append(countryCard)
+  })
+}
+
+
+searchInput.addEventListener('input',  (e) => {
+  const filteredCountries = allCountriesData.filter((country) => country.name.common.toLowerCase().includes(e.target.value.toLowerCase()))
+  console.log(filteredCountries);
+  renderCountries(filteredCountries)
+})
+
+themeChanger.addEventListener('click', () => {
+  document.body.classList.toggle('dark')
+})
